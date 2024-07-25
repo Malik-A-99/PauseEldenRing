fa9ea3fba68dc9f7984982f050545fcc23bc6155
Malik-A-99 <m.alimohamed23@gmail.com>
2024-07-24 22:51:08 -0700

    Update PauseEldenRing.py

--- PauseEldenRing.py
+++ PauseEldenRing.py
@@ -16,16 +16,26 @@ def pauseMenu():
     ctk.set_default_color_theme('dark-blue')
 
     root = ctk.CTk()
+
     root.geometry('320x240+' + str(posX) + '+' + str(posY))
 
-    label = ctk.CTkLabel(root, text='Game has been paused!')
+    def buttonTest():
+        root.destroy()
+
+    font = ('Times New Roman', 24)
+
+    label = ctk.CTkLabel(root, text='Game has been paused!', font=font, bg_color='black')
     label.place(relx=0.5, rely=0.4, anchor = ctk.CENTER)
 
-    button = ctk.CTkButton(root, text='~Resume~', command=root.destroy)
+    button = ctk.CTkButton(root, text='~Resume~', command=buttonTest, font=font, bg_color='#958051', fg_color='#958051', hover_color='#958051')
     button.place(relx=0.5, rely=0.6, anchor = ctk.CENTER)
 
     root.wm_attributes('-topmost', 1)
+    root.wm_attributes('-topmost', 1)
+    root.wm_attributes('-alpha', 0.9)
+    root.config(background='black', highlightbackground='#958051', highlightthickness=4, highlightcolor='#958051')
     root.overrideredirect(True)
+
     root.mainloop()
 
 
